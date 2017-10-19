19741ccd4841fd685d2be988a469e0838ee1ea74
Eric Prom <eric@kobidigital.com>
2017-10-19 22:15:54 +0700

    no message

--- resources/assets/js/components/index.js
+++ resources/assets/js/components/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Child from './Child'
 import DataViewer from './DataViewer'
 import Datepicker from './Datepicker';
-import vSelect from "vue-select"
+import vSelect from 'vue-select'
 
 Vue.component(Child.name, Child)
 Vue.component(DataViewer.name, DataViewer)
