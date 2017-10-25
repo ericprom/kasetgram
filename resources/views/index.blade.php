f4bd6239ef4d2604eeb7f1698b8c85e3b6469216
Eric Prom <eric@kobidigital.com>
2017-10-25 11:52:25 +0700

    no message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
   <title>{{ config('app.name') }}</title>
   <link rel="stylesheet" href="{{ asset(elixir('css/app.css')) }}"/>
 </head>
-<body class="hold-transition skin-blue fixed sidebar-mini">
+<body class="hold-transition skin-blue fixed sidebar-mini" style="background-color: #ecf0f5;">
 	<div id="app"></div>
 	@include('scripts')
 </body>
